cb35785ba6fa0af217d222af0c7cab63d42b9671
Tobias Aihonen <ta223dh@student.lnu.se>
2023-09-26 17:09:31 +0200

    Fix lint issues

--- src/TextBuddy/textbuddy.js
+++ src/TextBuddy/textbuddy.js
@@ -1,150 +1,161 @@
+/**
+ *
+ */
 class TextBuddy {
-    #text
-    #wordCount
+  #text
+  #wordCount
 
-    constructor (text) {
-        this.#text = text
-        this.#wordCount = null
+  /**
+   *
+   * @param text
+   */
+  constructor (text) {
+    this.#text = text
+    this.#wordCount = null
+  }
+
+  /**
+   *
+   */
+  fullAnalyzis () {
+    return {
+      'Word count': this.wordCount(),
+      'Character count': this.characterCount(),
+      'Character count excluding spaces': this.characterCountExcludingSpaces(),
+      'Word frequency': this.wordFrequency(),
+      'Unique word count': this.uniqueWordCount(),
+      'Longest word': this.longestWord(),
+      'Average word length': this.averageWordLength(),
+      'Estimated reading time in minutes': this.esimatedReadingTimeInMinutes()
     }
+  }
 
-    fullAnalyzis () {
-        return {
-            "Word count" : this.wordCount(),
-            "Character count": this.characterCount(),
-            "Character count excluding spaces": this.characterCountExcludingSpaces(),
-            "Word frequency": this.wordFrequency(),
-            "Unique word count": this.uniqueWordCount(),
-            "Longest word": this.longestWord(),
-            "Average word length": this.averageWordLength(),
-            "Estimated reading time in minutes": this.esimatedReadingTimeInMinutes()
-        }
-    }
+  /**
+   *
+   */
+  getText () {
+    return this.#text
+  }
 
-    getText () {
-        return this.#text
-    }
-
-    /**
-     * Returns the word count.
-     *
-     * @returns {number} The number of words in the text.
-     */
-    wordCount () {
-        if (!this.#wordCount) {
-            if (this.#text.trim().length === 0) {
-                this.#wordCount = 0 
-            } else {
-                const spaces = /\s+/g // Regex
-                this.#wordCount = this.#text.trim().replace(spaces, ' ').split(' ').length    
-            }
-        }
-        return this.#wordCount
-    }
-
-    /**
-     * Returns the character count.
-     *
-     * @returns {number} The number of characters in the text.
-     */
-    characterCount () {
-        return this.#text.length
-    }
-
-    /**
-     * Returns the character count excluding spaces.
-     *
-     * @returns {number} The number of characters (excluding spaces) in the text.
-     */
-    characterCountExcludingSpaces () {
-        return this.#text.replaceAll(' ', '').length
-    }
-
-    /**
-     * Returns a json object with each uniqe word and frequency of the word sorted with the most frequent word first.
-     *
-     * @returns {Object} A json object with each uniqe word and frequency of the word sorted by desc freq.
-     */
-    wordFrequency () {
-        if(this.#text.trim().length === 0)
-        return {}
-
+  /**
+   * Returns the word count.
+   *
+   * @returns {number} The number of words in the text.
+   */
+  wordCount () {
+    if (!this.#wordCount) {
+      if (this.#text.trim().length === 0) {
+        this.#wordCount = 0
+      } else {
         const spaces = /\s+/g // Regex
-        const words = this.#text.trim().toLowerCase().replace(spaces, ' ').split(' ')
+        this.#wordCount = this.#text.trim().replace(spaces, ' ').split(' ').length
+      }
+    }
+    return this.#wordCount
+  }
 
-        let wordFrequency = {}
+  /**
+   * Returns the character count.
+   *
+   * @returns {number} The number of characters in the text.
+   */
+  characterCount () {
+    return this.#text.length
+  }
 
-        words.forEach(word => {
-            if (!wordFrequency[word]) {
-                wordFrequency[word] = 1
-            } else {
-                wordFrequency[word] += 1
-            }
-        })
+  /**
+   * Returns the character count excluding spaces.
+   *
+   * @returns {number} The number of characters (excluding spaces) in the text.
+   */
+  characterCountExcludingSpaces () {
+    return this.#text.replaceAll(' ', '').length
+  }
 
-        const array = Object.entries(wordFrequency)
-        const sortedArray = array.sort((a, b) => b[1] - a[1])
-        const sortedWordFrequency = Object.fromEntries(sortedArray)
+  /**
+   * Returns a json object with each uniqe word and frequency of the word sorted with the most frequent word first.
+   *
+   * @returns {object} A json object with each uniqe word and frequency of the word sorted by desc freq.
+   */
+  wordFrequency () {
+    if (this.#text.trim().length === 0) { return {} }
 
-        return sortedWordFrequency
+    const spaces = /\s+/g // Regex
+    const words = this.#text.trim().toLowerCase().replace(spaces, ' ').split(' ')
+
+    const wordFrequency = {}
+
+    words.forEach(word => {
+      if (!wordFrequency[word]) {
+        wordFrequency[word] = 1
+      } else {
+        wordFrequency[word] += 1
+      }
+    })
+
+    const array = Object.entries(wordFrequency)
+    const sortedArray = array.sort((a, b) => b[1] - a[1])
+    const sortedWordFrequency = Object.fromEntries(sortedArray)
+
+    return sortedWordFrequency
+  }
+
+  /**
+   * Returns number representing the number of unique words.
+   *
+   * @returns {number} The number of unique words that occur in the text.
+   */
+  uniqueWordCount () {
+    const wordFrequency = this.wordFrequency(this.#text)
+    return Object.keys(wordFrequency).length
+  }
+
+  /**
+   * Returns the longest word as a string.
+   *
+   * @returns {string} The longest word that occur in the text.
+   */
+  longestWord () {
+    const wordFrequency = this.wordFrequency(this.#text)
+    let longestWord = ''
+
+    for (const word in wordFrequency) {
+      if (longestWord.length < word.length) {
+        longestWord = word
+      }
+    }
+    return longestWord
+  }
+
+  /**
+   * Returns the average word length as a number.
+   *
+   * @returns {string} The average length of all the words that occur in the text.
+   */
+  averageWordLength () {
+    if (this.#text.trim().length === 0) { return 0 }
+
+    const wordFrequency = this.wordFrequency(this.#text)
+    let numberOfWords = 0
+    let combinedLength = 0
+
+    for (const [key, value] of Object.entries(wordFrequency)) {
+      combinedLength += key.length * value
+      numberOfWords += value
     }
 
-    /**
-     * Returns number representing the number of unique words.
-     *
-     * @returns {number} The number of unique words that occur in the text.
-     */
-      uniqueWordCount() {
-        const wordFrequency = this.wordFrequency(this.#text)
-        return Object.keys(wordFrequency).length
-    }
+    return combinedLength / numberOfWords
+  }
 
-    /**
-     * Returns the longest word as a string.
-     *
-     * @returns {string} The longest word that occur in the text.
-     */
-      longestWord() {
-        const wordFrequency = this.wordFrequency(this.#text)
-        let longestWord = ''
-
-        for (let word in wordFrequency) {
-            if (longestWord.length < word.length) {
-                longestWord = word
-            }
-        }
-        return longestWord
-    }
-
-    /**
-     * Returns the average word length as a number.
-     *
-     * @returns {string} The average length of all the words that occur in the text.
-     */
-      averageWordLength() {
-        if(this.#text.trim().length === 0)
-        return 0
-
-        const wordFrequency = this.wordFrequency(this.#text)
-        let numberOfWords = 0
-        let combinedLength = 0
-
-        for (let [key, value] of Object.entries(wordFrequency)) {
-            combinedLength += key.length * value
-            numberOfWords += value
-        }
-
-        return combinedLength / numberOfWords
-    }
-
-    /**
-     * Returns the estimated reading time in minutes as a number.
-     *
-     * @returns {number} The estimated reading time in minutes.
-     */
-    esimatedReadingTimeInMinutes() {
-        /* Assumed 200 words per minute average speed */
-        return this.wordCount() / 200
-    }
+  /**
+   * Returns the estimated reading time in minutes as a number.
+   *
+   * @returns {number} The estimated reading time in minutes.
+   */
+  esimatedReadingTimeInMinutes () {
+    /* Assumed 200 words per minute average speed */
+    return this.wordCount() / 200
+  }
 }
 
 export default TextBuddy
--- src/tests/textbuddy.test.js
+++ src/tests/textbuddy.test.js
@@ -6,10 +6,14 @@ import TextBuddy from '../TextBuddy/textbuddy.js'
  * Try invalid input values()
  */
 test('Invalid values', t => {
+  /**
+   *
+   * @param inputFunction
+   */
   function tryInvalidValues (inputFunction) {
     try {
       inputFunction()
-      t.fail() 
+      t.fail()
     } catch (error) {
       t.pass()
     }
@@ -19,9 +23,9 @@ test('Invalid values', t => {
   tryInvalidValues(new TextBuddy(null))
   tryInvalidValues(new TextBuddy([]))
   tryInvalidValues(new TextBuddy({}))
-  tryInvalidValues(new TextBuddy(new String))
-  tryInvalidValues(new TextBuddy(new Number))
-  tryInvalidValues(new TextBuddy(new Error))
+  tryInvalidValues(new TextBuddy(new String()))
+  tryInvalidValues(new TextBuddy(new Number()))
+  tryInvalidValues(new TextBuddy(new Error()))
 })
 
 /**
@@ -29,29 +33,30 @@ test('Invalid values', t => {
  * wordCount()
  */
 test('Word count', t => {
-    new TextBuddy('').wordCount() === 0 ? t.pass() : t.fail() 
-    new TextBuddy('OneWord').wordCount() === 1 ? t.pass() : t.fail() 
-    new TextBuddy('Two Words').wordCount() === 2 ? t.pass() : t.fail() 
-    new TextBuddy('       Three Different Words').wordCount() === 3 ? t.pass() : t.fail() 
-    new TextBuddy('!Two        Words_').wordCount() === 2 ? t.pass() : t.fail() 
-    new TextBuddy('OneWord          ').wordCount() === 1 ? t.pass() : t.fail() 
-    new TextBuddy('Five Words . , *').wordCount() === 5 ? t.pass() : t.fail() 
-    new TextBuddy('Six Words six words SIX WORDS').wordCount() === 6 ? t.pass() : t.fail() 
-    new TextBuddy('SEVEN SEVEN SEVEN SEVEN SEVEN SEVEN SEVEN').wordCount() === 7 ? t.pass() : t.fail() 
-    //new TextBuddy('\n\n').getWordCount() === 0 ? t.pass() : t.fail() 
-    //new TextBuddy('     ').getWordCount() === 0 ? t.pass() : t.fail() 
-  })
+  new TextBuddy('').wordCount() === 0 ? t.pass() : t.fail()
+  new TextBuddy('OneWord').wordCount() === 1 ? t.pass() : t.fail()
+  new TextBuddy('Two Words').wordCount() === 2 ? t.pass() : t.fail()
+  new TextBuddy('       Three Different Words').wordCount() === 3 ? t.pass() : t.fail()
+  new TextBuddy('!Two        Words_').wordCount() === 2 ? t.pass() : t.fail()
+  new TextBuddy('OneWord          ').wordCount() === 1 ? t.pass() : t.fail()
+  new TextBuddy('Five Words Five Words Five').wordCount() === 5 ? t.pass() : t.fail()
+  new TextBuddy('Six Words six words SIX WORDS').wordCount() === 6 ? t.pass() : t.fail()
+  new TextBuddy('SEVEN SEVEN SEVEN SEVEN SEVEN SEVEN SEVEN').wordCount() === 7 ? t.pass() : t.fail()
+
+  // new TextBuddy('\n\n').getWordCount() === 0 ? t.pass() : t.fail()
+  // new TextBuddy('     ').getWordCount() === 0 ? t.pass() : t.fail()
+})
 
 /**
  * Test 3
  * getCharacterCount()
  */
 test('Character count', t => {
-  new TextBuddy('').characterCount() === 0 ? t.pass() : t.fail() 
-  new TextBuddy(' ').characterCount() === 1 ? t.pass() : t.fail() 
-  new TextBuddy('!').characterCount() === 1 ? t.pass() : t.fail() 
-  new TextBuddy('Four').characterCount() === 4 ? t.pass() : t.fail() 
-  new TextBuddy('Five ').characterCount() === 5 ? t.pass() : t.fail() 
+  new TextBuddy('').characterCount() === 0 ? t.pass() : t.fail()
+  new TextBuddy(' ').characterCount() === 1 ? t.pass() : t.fail()
+  new TextBuddy('!').characterCount() === 1 ? t.pass() : t.fail()
+  new TextBuddy('Four').characterCount() === 4 ? t.pass() : t.fail()
+  new TextBuddy('Five ').characterCount() === 5 ? t.pass() : t.fail()
 })
 
 /**
@@ -59,11 +64,11 @@ test('Character count', t => {
  * characterCountExcludingSpaces()
  */
 test('Character count exluding spaces', t => {
-  new TextBuddy('').characterCountExcludingSpaces() === 0 ? t.pass() : t.fail() 
-  new TextBuddy(' ').characterCountExcludingSpaces() === 0 ? t.pass() : t.fail() 
-  new TextBuddy('!').characterCountExcludingSpaces() === 1 ? t.pass() : t.fail() 
-  new TextBuddy('Four').characterCountExcludingSpaces() === 4 ? t.pass() : t.fail() 
-  new TextBuddy('Five ').characterCountExcludingSpaces() === 4 ? t.pass() : t.fail() 
+  new TextBuddy('').characterCountExcludingSpaces() === 0 ? t.pass() : t.fail()
+  new TextBuddy(' ').characterCountExcludingSpaces() === 0 ? t.pass() : t.fail()
+  new TextBuddy('!').characterCountExcludingSpaces() === 1 ? t.pass() : t.fail()
+  new TextBuddy('Four').characterCountExcludingSpaces() === 4 ? t.pass() : t.fail()
+  new TextBuddy('Five ').characterCountExcludingSpaces() === 4 ? t.pass() : t.fail()
 })
 
 /**
@@ -71,13 +76,18 @@ test('Character count exluding spaces', t => {
  * wordFrequency()
  */
 test('Word frequency', t => {
-  compare(new TextBuddy('').wordFrequency(), {}) ? t.pass() : t.fail() 
-  compare(new TextBuddy(' ').wordFrequency(), {}) ? t.pass() : t.fail() 
-  compare(new TextBuddy('One').wordFrequency(), {one: 1}) ? t.pass() : t.fail() 
-  compare(new TextBuddy('Two two').wordFrequency(), {two: 2}) ? t.pass() : t.fail() 
-  compare(new TextBuddy('One two two').wordFrequency(), {two: 2, one: 1}) ? t.pass() : t.fail() 
+  compare(new TextBuddy('').wordFrequency(), {}) ? t.pass() : t.fail()
+  compare(new TextBuddy(' ').wordFrequency(), {}) ? t.pass() : t.fail()
+  compare(new TextBuddy('One').wordFrequency(), { one: 1 }) ? t.pass() : t.fail()
+  compare(new TextBuddy('Two two').wordFrequency(), { two: 2 }) ? t.pass() : t.fail()
+  compare(new TextBuddy('One two two').wordFrequency(), { two: 2, one: 1 }) ? t.pass() : t.fail()
 
-  function compare(objectOne, objectTwo) {
+  /**
+   *
+   * @param objectOne
+   * @param objectTwo
+   */
+  function compare (objectOne, objectTwo) {
     return Object.entries(objectOne).toString() === Object.entries(objectTwo).toString()
   }
 })
@@ -87,11 +97,11 @@ test('Word frequency', t => {
  * uniqueWordCount()
  */
 test('Number of unique words', t => {
-  new TextBuddy('').uniqueWordCount() === 0 ? t.pass() : t.fail() 
-  new TextBuddy('One').uniqueWordCount() === 1 ? t.pass() : t.fail() 
-  new TextBuddy('One one').uniqueWordCount() === 1 ? t.pass() : t.fail() 
-  new TextBuddy('One Two Three Four Five').uniqueWordCount() === 5 ? t.pass() : t.fail() 
-  new TextBuddy('One one two two three three').uniqueWordCount() === 3 ? t.pass() : t.fail() 
+  new TextBuddy('').uniqueWordCount() === 0 ? t.pass() : t.fail()
+  new TextBuddy('One').uniqueWordCount() === 1 ? t.pass() : t.fail()
+  new TextBuddy('One one').uniqueWordCount() === 1 ? t.pass() : t.fail()
+  new TextBuddy('One Two Three Four Five').uniqueWordCount() === 5 ? t.pass() : t.fail()
+  new TextBuddy('One one two two three three').uniqueWordCount() === 3 ? t.pass() : t.fail()
 })
 
 /**
@@ -99,11 +109,11 @@ test('Number of unique words', t => {
  * longestWord()
  */
 test('Longest word', t => {
-  new TextBuddy('').longestWord() === '' ? t.pass() : t.fail() 
-  new TextBuddy(' ').longestWord() === '' ? t.pass() : t.fail() 
-  new TextBuddy('One one').longestWord() === 'one' ? t.pass() : t.fail() 
-  new TextBuddy('One Two Three Four Five').longestWord() === 'three' ? t.pass() : t.fail() 
-  new TextBuddy('This is a long word: helicopter').longestWord() === 'helicopter' ? t.pass() : t.fail() 
+  new TextBuddy('').longestWord() === '' ? t.pass() : t.fail()
+  new TextBuddy(' ').longestWord() === '' ? t.pass() : t.fail()
+  new TextBuddy('One one').longestWord() === 'one' ? t.pass() : t.fail()
+  new TextBuddy('One Two Three Four Five').longestWord() === 'three' ? t.pass() : t.fail()
+  new TextBuddy('This is a long word: helicopter').longestWord() === 'helicopter' ? t.pass() : t.fail()
 })
 
 /**
@@ -111,10 +121,10 @@ test('Longest word', t => {
  * averageWordLength()
  */
 test('Average word length', t => {
-  new TextBuddy('').averageWordLength() === 0 ? t.pass() : t.fail() 
-  new TextBuddy(' ').averageWordLength() === 0 ? t.pass() : t.fail() 
-  new TextBuddy('Four four four four').averageWordLength() === 4 ? t.pass() : t.fail() 
-  new TextBuddy('The average is').averageWordLength() === 4 ? t.pass() : t.fail() 
+  new TextBuddy('').averageWordLength() === 0 ? t.pass() : t.fail()
+  new TextBuddy(' ').averageWordLength() === 0 ? t.pass() : t.fail()
+  new TextBuddy('Four four four four').averageWordLength() === 4 ? t.pass() : t.fail()
+  new TextBuddy('The average is').averageWordLength() === 4 ? t.pass() : t.fail()
 })
 
 /**
@@ -122,10 +132,8 @@ test('Average word length', t => {
  * estimatedReadingTimeInMinutes()
  */
 test('Estimated reading time in minutes', t => {
-  new TextBuddy('').esimatedReadingTimeInMinutes() === 0 ? t.pass() : t.fail() 
-  new TextBuddy(' ').esimatedReadingTimeInMinutes() === 0 ? t.pass() : t.fail() 
-  new TextBuddy('One').esimatedReadingTimeInMinutes() === 0.005 ? t.pass() : t.fail() 
-  new TextBuddy('happened creditcard tyan huf henrietta thomas rational dopod springhill woven foundational hometwat muzzle esse breakfast skilling updates polymerase newbie arrangement ihc xvii jar zithromax aix archivists crust powershot vivo dunwoody proposals annualized centrino touched rabble foals plank atty fielding violator gon thunderstorm unbundled rescues decipher fad concluded uploader carat gimp oyster flowering paid perle gof derrick faxpress techspot harmonics motogp gti urbanization thornton amphitheater heterosexual infect sounds escudo emptiness steelhead moda nonspecific malay turtleneck labelle followed smyth sight delicately foresee answer vijay faulkner pentium babysitter naughty handicapper sbs graf pissed rangers cancers consonants drama gothenburg imprinted commercially disgrace smashing conjure castles westgate compressor sions golan subscription transformers collaboration naruto generics arise compassion eager ivillage straight discounted grasses circumstances haney atherton montgomery cardboard classpath freezing degenerative proverb swedish webster obedience superior jewellery albanians trendnet motrin antcn fu fraught driver subways dragoon condemned tendered important nick irt touchstone spongebob draper clumps occupy moblog msb scrubbed roh scaffold quickly blakely flavour passports packer bermuda hypocritical gorillas oboe nrc inspectorate html happiest nakamura boardhost coleman they difficult pedersen cooper iota talking clan objections intercity canada hardcore sobbing deactivate me brow stamina quod competitor bmj pesca labtec mattress hookups landowners petitioner renee flop abrahams frighten').esimatedReadingTimeInMinutes() === 1 ? t.pass() : t.fail() 
+  new TextBuddy('').esimatedReadingTimeInMinutes() === 0 ? t.pass() : t.fail()
+  new TextBuddy(' ').esimatedReadingTimeInMinutes() === 0 ? t.pass() : t.fail()
+  new TextBuddy('One').esimatedReadingTimeInMinutes() === 0.005 ? t.pass() : t.fail()
+  new TextBuddy('happened creditcard tyan huf henrietta thomas rational dopod springhill woven foundational hometwat muzzle esse breakfast skilling updates polymerase newbie arrangement ihc xvii jar zithromax aix archivists crust powershot vivo dunwoody proposals annualized centrino touched rabble foals plank atty fielding violator gon thunderstorm unbundled rescues decipher fad concluded uploader carat gimp oyster flowering paid perle gof derrick faxpress techspot harmonics motogp gti urbanization thornton amphitheater heterosexual infect sounds escudo emptiness steelhead moda nonspecific malay turtleneck labelle followed smyth sight delicately foresee answer vijay faulkner pentium babysitter naughty handicapper sbs graf pissed rangers cancers consonants drama gothenburg imprinted commercially disgrace smashing conjure castles westgate compressor sions golan subscription transformers collaboration naruto generics arise compassion eager ivillage straight discounted grasses circumstances haney atherton montgomery cardboard classpath freezing degenerative proverb swedish webster obedience superior jewellery albanians trendnet motrin antcn fu fraught driver subways dragoon condemned tendered important nick irt touchstone spongebob draper clumps occupy moblog msb scrubbed roh scaffold quickly blakely flavour passports packer bermuda hypocritical gorillas oboe nrc inspectorate html happiest nakamura boardhost coleman they difficult pedersen cooper iota talking clan objections intercity canada hardcore sobbing deactivate me brow stamina quod competitor bmj pesca labtec mattress hookups landowners petitioner renee flop abrahams frighten').esimatedReadingTimeInMinutes() === 1 ? t.pass() : t.fail()
 })
-
-
